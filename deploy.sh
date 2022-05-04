deb9dbce63758499f3efb269c0807e7d5fc5ceae
Noko Lautze <insideclass99@gmail.com>
2022-05-04 22:40:47 +0200

    changed project path

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ deploy.sh
   git init
   git add -A
   git commit -m "New Deployment"
-  git push -f git@github.com:logoxo/koomz_miniting_page.git master:gh-pages
+  git push -f git@github.com:logoxo/koomz_minting_page.git master:gh-pages
 
   cd - 
